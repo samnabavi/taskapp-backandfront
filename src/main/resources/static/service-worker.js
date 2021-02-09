ba10165e7394e722b40d6ff9d622f0f4a32333f0
samnabavi <snabavi@ucsd.edu>
2021-02-09 09:40:51 -0800

    update

--- src/main/resources/static/service-worker.js
+++ src/main/resources/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4aa0080c12546fdcf1e9d909af476982.js"
+  "/precache-manifest.93b64227af146f1d9436ce3676fb700c.js"
 );
 
 self.addEventListener('message', (event) => {
